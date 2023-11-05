2d4b417a6332c94600d70c9d311e55f5f526b315
Gabe C <113395303+GabeVC@users.noreply.github.com>
2023-11-05 10:28:29 -0500

    Update flask_stuff.py

--- flask_stuff.py
+++ flask_stuff.py
@@ -8,6 +8,7 @@ CORS(app)
 
 # Assuming we have a list to store our patient objects
 patients_list = []
+output = ''
 
 @app.route('/process_data', methods=['POST'])
 def process_data():
@@ -36,7 +37,7 @@ def process_data():
 
         # You may want to add functionality to calculate and set the score here
         #use main.Run
-        main.run(new_patient)
+        output = main.run(new_patient)
 
         return jsonify({'message': 'Patient data received and processed successfully.'}), 200
     except Exception as e:
@@ -46,16 +47,30 @@ def process_data():
 def get_patients():
     try:
         # Serialize the patient data to send as JSON
+        
+
         patients_data = [{
             'name': patient.get_name(),
             'age': patient.get_age(),
             'severity': patient.get_severity(),
-            'symptoms_duration': patient.get_list_of_symptoms_durations()
+            'symptoms_duration': patient.get_list_of_symptoms_durations(),
+            'score': patient.get_score(),
+            'pastHistory' : patient.patient_history
         } for patient in patients_list]
 
         return jsonify(patients_data), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+@app.route('/delete', methods=['POST'])
+def delete():
+    data = request.json
+    try:
+        print("hi")
+        id_to_delete = data['id']
+        return jsonify({'message': 'Data deleted successfully'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
 if __name__ == '__main__':
     app.run(debug=True)
